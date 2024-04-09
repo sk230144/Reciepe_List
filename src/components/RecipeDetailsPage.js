cdcbdd5a4d0017ebdd75b7c80878424995ec650e
Saurabh Tiwari <risabht043@gmail.com>
2024-04-09 04:27:05 -0400

    Make new features

--- src/components/RecipeDetailsPage.js
+++ src/components/RecipeDetailsPage.js
@@ -1,15 +1,18 @@
-// RecipeDetailsPage.js
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom'; // Import Link
 import './RecipeDetailsPage.css'; // Import the CSS file
 
 const RecipeDetailsPage = () => {
   const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
+  const [rating, setRating] = useState(() => {
+    return parseInt(localStorage.getItem(`recipe_${id}_rating`) || 0);
+  });
+  const [submitted, setSubmitted] = useState(false);
 
   useEffect(() => {
     fetchRecipeDetails();
-  }, [id]);
+  }, [id, submitted]);
 
   const fetchRecipeDetails = async () => {
     try {
@@ -21,36 +24,70 @@ const RecipeDetailsPage = () => {
     }
   };
 
+  const handleRatingChange = (event) => {
+    setRating(parseInt(event.target.value));
+  };
+
+  const handleSubmitRating = (event) => {
+    event.preventDefault();
+    try {
+      // Save the rating to localStorage
+      localStorage.setItem(`recipe_${id}_rating`, rating);
+      setSubmitted(true);
+      alert("Your review is helpful for us, thanks!")
+    } catch (error) {
+      console.error('Error submitting rating:', error);
+    }
+  };
+
   if (!recipe) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="recipe-details-container">
-      <h1 className="recipe-title">{recipe.title}</h1>
-      <p className="recipe-info">Ready in: {recipe.readyInMinutes} minutes</p>
-      <p className="recipe-info">Servings: {recipe.servings}</p>
-      <p className="recipe-source">
-        Source: <a href={recipe.sourceUrl} target="_blank" rel="noopener noreferrer">{recipe.sourceName}</a>
-      </p>
-      <img src={`https://spoonacular.com/recipeImages/${recipe.image}`} alt={recipe.title} className="recipe-image" />
-      <h2 className="section-title">Ingredients:</h2>
-      <ul className="ingredient-list">
-        {recipe.extendedIngredients.map(ingredient => (
-          <li key={ingredient.id}>{ingredient.original}</li>
-        ))}
-      </ul>
-      <h2 className="section-title">Instructions:</h2>
-      <ol className="instruction-list">
-        {recipe.analyzedInstructions && recipe.analyzedInstructions.length > 0 ? (
-          recipe.analyzedInstructions[0].steps.map(step => (
-            <li key={step.number}>{step.step}</li>
-          ))
-        ) : (
-          <li>No instructions available.</li>
-        )}
-      </ol>
-    </div>
+    <>
+      <h1 style={{ textAlign: 'center' }}>Recipe Details</h1>
+      <div className="recipe-details-container">
+      <div className='home-link'>
+      <Link to="/">Go Back</Link> {/* Add home button */}
+      </div>
+        
+        <h1 className="recipe-title">{recipe.title}</h1>
+        <p className="recipe-info">Ready in: {recipe.readyInMinutes} minutes</p>
+        <p className="recipe-info">Servings: {recipe.servings}</p>
+        <p className="recipe-source">
+          Source: <a href={recipe.sourceUrl} target="_blank" rel="noopener noreferrer">{recipe.sourceName}</a>
+        </p>
+        <img src={`https://spoonacular.com/recipeImages/${recipe.image}`} alt={recipe.title} className="recipe-image" />
+        <h2 className="section-title">Ingredients:</h2>
+        <ul className="ingredient-list">
+          {recipe.extendedIngredients.map(ingredient => (
+            <li key={ingredient.id}>{ingredient.original}</li>
+          ))}
+        </ul>
+        <h2 className="section-title">Instructions:</h2>
+        <ol className="instruction-list">
+          {recipe.analyzedInstructions && recipe.analyzedInstructions.length > 0 ? (
+            recipe.analyzedInstructions[0].steps.map(step => (
+              <li key={step.number}>{step.step}</li>
+            ))
+          ) : (
+            <li>No instructions available.</li>
+          )}
+        </ol>
+        <h2 className="section-title">Rate this recipe:</h2>
+        <form onSubmit={handleSubmitRating}>
+          <input 
+            type="number" 
+            min="1" 
+            max="5" 
+            value={rating} 
+            onChange={handleRatingChange} 
+          />
+          <button type="submit">Submit Rating</button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- src/components/RecipeListPage.js
+++ src/components/RecipeListPage.js
@@ -1,41 +1,82 @@
-// RecipeListPage.js
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './RecipeListPage.css'; // Import the CSS file
 
 const RecipeListPage = () => {
   const [recipes, setRecipes] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchRecipes();
-  }, []);
+  }, [selectedCategory]);
 
   const fetchRecipes = async () => {
     try {
-      const response = await fetch('https://api.spoonacular.com/recipes/search?query=pasta&number=10&apiKey=eb25ac4e282d40e887d6bf3778c501f2');
+      setLoading(true);
+      let url = `https://api.spoonacular.com/recipes/search?number=10&apiKey=eb25ac4e282d40e887d6bf3778c501f2`;
+      if (searchQuery) {
+        url += `&query=${searchQuery}`;
+      }
+      if (selectedCategory) {
+        url += `&cuisine=${selectedCategory}`;
+      }
+      const response = await fetch(url);
       const data = await response.json();
       setRecipes(data.results);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching recipes:', error);
     }
   };
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    fetchRecipes();
+  };
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
   return (
     <div className="main-container">
-      <h1 className="recipe-list-title">Recipe List</h1>
-      <ul className="recipe-list">
-        {recipes.map(recipe => (
-          <li key={recipe.id} className="recipe-item">
-            <Link to={`/recipes/${recipe.id}`}>
-              <div className="recipe-card">
-                <img src={`https://spoonacular.com/recipeImages/${recipe.image}`} alt={recipe.title} className="recipe-image" />
-                <p className="recipe-title">{recipe.title}</p>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <h1 style={{ textAlign: 'center' }}>Recipe List</h1>
+      <form onSubmit={handleSearchSubmit} className="search-form">
+        <input type="text" value={searchQuery} onChange={handleSearchChange} placeholder="Search by name or ingredients" className="search-input" />
+        <button type="submit" className="search-button">Search</button>
+      </form>
+      <div className="filter-container">
+        <label htmlFor="category">Filter by Category:</label>
+        <select id="category" value={selectedCategory} onChange={handleCategoryChange}>
+          <option value="">All</option>
+          <option value="italian">Italian</option>
+          <option value="mexican">Mexican</option>
+          <option value="indian">Indian</option>
+          {/* Add more options as needed */}
+        </select>
+      </div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="recipe-list">
+          {recipes.map(recipe => (
+            <li key={recipe.id} className="recipe-item">
+              <Link to={`/recipes/${recipe.id}`}>
+                <div className="recipe-card">
+                  <img src={`https://spoonacular.com/recipeImages/${recipe.image}`} alt={recipe.title} className="recipe-image" />
+                  <p className="recipe-title">{recipe.title}</p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
